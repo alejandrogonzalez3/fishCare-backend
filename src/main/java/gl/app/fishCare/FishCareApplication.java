e2905bfca193249425040c40853677c801977a02
Alejandro <alejandro.gonzalez3@udc.es>
2021-03-19 20:41:53 +0100

    [FIRST COMMIT] Base project (Spring initializr)

---
+++ src/main/java/gl/app/fishCare/FishCareApplication.java
@@ -0,0 +1,13 @@
+package gl.app.fishCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gl/app/fishCare/FishCareApplicationTests.java
@@ -0,0 +1,13 @@
+package gl.app.fishCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
