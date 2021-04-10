6db11b0aa9dd9e6e11ada7361cbe86462967bd5e
Alejandro <alejandro.gonzalez3@udc.es>
2021-04-10 18:17:46 +0200

    [*] Swagger support: * Swagger and Swagger UI dependencies (pom) * Use @ApiOperation annotation * Change README * Modified ExampleController

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 ### Backend Spring Boot
 
 * mvn spring-boot:run
+
+## Utilización
+
+Se utiliza Swagger y Swagger UI para probar los Endpoints. Accediendo a la siguiente dirección: localhost:8080/swagger-ui.html
--- src/main/java/gl/app/fishCare/controller/ExampleController.java
+++ src/main/java/gl/app/fishCare/controller/ExampleController.java
@@ -1,12 +1,17 @@
 package gl.app.fishCare.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@RequestMapping("/example")
 public class ExampleController {
 
-	@RequestMapping("/")
+	@ApiOperation(value = "Petición GET de ejemplo")
+	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
